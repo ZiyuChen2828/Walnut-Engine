24fb777611f0d320e5f0105e4cf138de9e208d0f
Ziyu Chen <zychen2828@gmail.com>
2022-02-24 22:51:06 -0800

    setup premake file

---
+++ premake5.lua
@@ -0,0 +1,107 @@
+workspace "Walnut"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Walnut"
+	location "Walnut"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Walnut/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"WN_PLATFORM_WINDOWS",
+			"WN_BUILD_DLL"
+		}
+		
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "WN_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "WN_RELEASE"
+		optimize "On"
+
+	filter "configurations:Debug"
+		defines "WN_DIST"
+		optimize "On"
+	
+
+project "Sandbox"
+	location "Sandbox"
+
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Walnut/vendor/spdlog/include",
+		"Walnut/src"
+	}
+
+	links
+	{
+		"Walnut"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"WN_PLATFORM_WINDOWS",
+		}
+
+	filter "configurations:Debug"
+		defines "WN_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "WN_RELEASE"
+		optimize "On"
+
+	filter "configurations:Debug"
+		defines "WN_DIST"
+		optimize "On"
